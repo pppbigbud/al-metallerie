f93104c6243e20f1b195b9dedc1580f3c225c00d
pppbigbud <pppbigbud@gmail.com>
2025-12-05 20:32:46 +0100

    Fix: supprimer fonction dupliquee almetal_disable_emojis

--- wordpress/wp-content/themes/almetal-theme/inc/performance-optimizer.php
+++ wordpress/wp-content/themes/almetal-theme/inc/performance-optimizer.php
@@ -137,19 +137,7 @@ function almetal_add_fetchpriority_to_lcp($attr, $attachment, $size) {
 }
 add_filter('wp_get_attachment_image_attributes', 'almetal_add_fetchpriority_to_lcp', 10, 3);
 
-/**
- * Désactiver les emojis WordPress (économie de requêtes)
- */
-function almetal_disable_emojis() {
-    remove_action('wp_head', 'print_emoji_detection_script', 7);
-    remove_action('admin_print_scripts', 'print_emoji_detection_script');
-    remove_action('wp_print_styles', 'print_emoji_styles');
-    remove_action('admin_print_styles', 'print_emoji_styles');
-    remove_filter('the_content_feed', 'wp_staticize_emoji');
-    remove_filter('comment_text_rss', 'wp_staticize_emoji');
-    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
-}
-add_action('init', 'almetal_disable_emojis');
+// Note: almetal_disable_emojis() est déjà définie dans functions.php
 
 /**
  * Supprimer les liens inutiles du head
